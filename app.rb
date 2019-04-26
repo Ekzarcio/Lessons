9e4b0a1f63c1143fa08e3da932daebe40184510d
Ekzarcio <evg1093@gmail.com>
2019-04-26 17:55:51 +0300

    All working

--- app.rb
+++ app.rb
@@ -21,21 +21,30 @@ post '/visit' do
 	@datetime = params[:datetime]
 	@barber = params[:barber]
 
-	if @username == ''
-		@error = 'Введите имя'
-		erb :visit
-	elsif @userphone == ''
-		@error = 'Введите номер телефона'
-		erb :visit
-	elsif @datetime == ''
-		@error = 'Введите дату и время'
-		erb :visit
-	else
-		f = File.open './public/users.txt', 'a'
-		f.write "User: #{@username}, phone: #{@userphone}, date & time: #{@datetime}, barber: #{@barber}\n"
-		f.close
-		erb "Уважаемый #{@username}, ждем Вас #{@datetime}"
+	hh = {  :username => 'Введите имя', 
+			:userphone => 'Введите номер телефона', 
+			:datetime => 'Введите дату и время'	}
+	hh.each do |key, value|
+		if params[key] == ''
+			@error = hh[key]
+			return erb :visit
+		end
 	end
+#	if @username == ''
+#		@error = 'Введите имя'
+#	end
+#	if @userphone == ''
+#		@error = 'Введите номер телефона'
+#	end
+#	if @datetime == ''
+#		@error = 'Введите дату и время'
+#	end
+#	if @error != ''
+#		erb :visit
+	f = File.open './public/users.txt', 'a'
+	f.write "User: #{@username}, phone: #{@userphone}, date & time: #{@datetime}, barber: #{@barber}\n"
+	f.close
+	erb "Уважаемый #{@username}, ждем Вас #{@datetime}"
 end
 
 get '/contacts' do
